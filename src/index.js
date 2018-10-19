0d47e42af7c226c6e72eee76d9878d7d93ea7d7b
raszidzie <charliescene512@gmail.com>
2018-10-19 05:57:37 -0400

    final update

--- src/index.js
+++ src/index.js
@@ -15,16 +15,21 @@ class App extends Component {
         this.state = { videos: [],
         selectedVideo: null
      };
+     this.videoSearch('yamaha r6')
 
-        YTSearch ({ key: API_KEY, term: 'yamahar1'}, (videos) => {
-           this.setState({ videos:videos,
-         selectedVideo:videos[0] });
-        });
+    
+    }
+    videoSearch(term){
+
+        YTSearch ({ key: API_KEY, term: term}, (videos) => {
+            this.setState({ videos:videos,
+          selectedVideo:videos[0] });
+         });
     }
     render() {
         return(
             <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
             <VideoDetail video={this.state.selectedVideo} />
             <VideoList 
              onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
