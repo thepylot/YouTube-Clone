ac0e7129c667073746d1a028b72f5a34ad122b3a
raszidzie <charliescene512@gmail.com>
2018-11-01 08:28:54 -0400

    dearch button

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,20 +10,39 @@ class SearchBar extends Component{
     }
     render(){
         return( 
-        <div className="search-bar">
+
+        <div className="search-bar row">
+        <div className="col-sm-10">
         <input 
-        value ={this.state.term}
-        onChange={event =>  this.onInputChange(event.target.value)} />
+        className="form-control col-sm-7"
+        onChange ={ event => 
+            this.setState({ value: event.target.value })}
+          
         
+        
+        placeholder="Search YouTube"/>
         </div>
+        <div className="col-sm-2">
+        <button 
+        value ={this.state.value}
+        onClick={event =>  this.onInputChange(event.target.value)} 
+        className="btn btn-danger col-sm-5">
+        <i className="fa fa-search">
+        </i>
+        
+        </button>
+        </div>
+        </div>
+        
         );
     }
 
    onInputChange(term){
       this.setState({term})
       this.props.onSearchTermChange(term);
+      
    }
-
+ 
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -27,6 +28,8 @@ class App extends Component {
          });
     }
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+
         return(
             <div>
             <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
